ed08ff6cf19d4ea475b0151ed383e7ba164add45
Arkadiy A.Davydov <jah.knows.me.well@gmail.com>
2022-03-08 21:40:48 +0300

    fix(PrideToken): change ticker to upper case

--- contracts/PrideToken.sol
+++ contracts/PrideToken.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "./RecoverableFunds.sol";
 
 contract PrideToken is ERC20Burnable, RecoverableFunds {
-    constructor() payable ERC20("PRIDE Token", "Pride") {
+    constructor() payable ERC20("PRIDE Token", "PRIDE") {
         _mint(_msgSender(), 250_000_000 ether);
     }
 }
