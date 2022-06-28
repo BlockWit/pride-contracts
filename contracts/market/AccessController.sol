c041a914970a43404c5b3b2255403d55153a7b7e
Arkadiy A.Davydov <davydov@blockwit.io>
2022-06-28 18:07:42 +0300

    chore(contracts/market): add setters/getters to controllers

--- contracts/market/AccessController.sol
+++ contracts/market/AccessController.sol
@@ -2,26 +2,49 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "../interfaces/IAccessController.sol";
 import "../RecoverableFunds.sol";
 import "./AccessLevels.sol";
 
-contract AccessController is IAccessController, RecoverableFunds {
+contract AccessController is IAccessController, RecoverableFunds, AccessControl {
 
     using AccessLevels for AccessLevels.Map;
 
-    mapping(address => uint8) private accountLevels;
+    mapping(address => uint8) public accountLevels;
     AccessLevels.Map private accessLevels;
 
-    function setAccountLevel(address[] calldata accounts, uint8[] calldata levels) external {
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function getAccessLevel(uint256 key) external view returns (AccessLevels.AccessLevel memory){
+        return accessLevels.get(key);
+    }
+
+    function getAccessLevelKeys() external view returns (uint256[] memory keys){
+        return accessLevels.keys();
+    }
+
+    function setAccessLevel(uint256 key, uint256 start, uint256 end) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (accessLevels.contains(key)) {
+            AccessLevels.AccessLevel storage level = accessLevels.get(key);
+            level.start = start;
+            level.end = start;
+        } else {
+            accessLevels.set(key, AccessLevels.AccessLevel(start, end));
+        }
+    }
+
+    function removeAccessLevel(uint256 key) external onlyRole(DEFAULT_ADMIN_ROLE) returns (bool) {
+        return accessLevels.remove(key);
+    }
+
+    function setAccountLevels(address[] calldata accounts, uint8[] calldata levels) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(accounts.length == levels.length, "Whitelist: Incorrect array length.");
         for (uint256 i = 0; i < accounts.length; i++) {
             accountLevels[accounts[i]] = levels[i];
         }
-    }
-
-    function getAccountLevel(address account) external view returns (uint8) {
-        return accountLevels[account];
     }
 
     function hasAccess(address account) override external view returns (bool) {
--- contracts/market/PricingController.sol
+++ contracts/market/PricingController.sol
@@ -2,16 +2,43 @@
 
 pragma solidity ^0.8.0;
 
-
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "../interfaces/IPricingController.sol";
 import "../RecoverableFunds.sol";
 import "./PricingStrategies.sol";
 
-contract PricingController is IPricingController,  RecoverableFunds {
+contract PricingController is IPricingController, RecoverableFunds, AccessControl {
 
     using PricingStrategies for PricingStrategies.Map;
 
     PricingStrategies.Map private pricingStrategies;
+
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function getPricingStrategy(uint256 key) external view returns (PricingStrategies.PricingStrategy memory){
+        return pricingStrategies.get(key);
+    }
+
+    function getPricingStrategyKeys() external view returns (uint256[] memory keys){
+        return pricingStrategies.keys();
+    }
+
+    function setPricingStrategy(uint256 key, uint256 start, uint256[] calldata intervals, uint256 priceStep) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (pricingStrategies.contains(key)) {
+            PricingStrategies.PricingStrategy storage strategy = pricingStrategies.get(key);
+            strategy.start = start;
+            strategy.intervals = intervals;
+            strategy.priceStep = priceStep;
+        } else {
+            pricingStrategies.set(key, PricingStrategies.PricingStrategy(start, intervals, priceStep));
+        }
+    }
+
+    function removePricingStrategy(uint256 key) external onlyRole(DEFAULT_ADMIN_ROLE) returns (bool) {
+        return pricingStrategies.remove(key);
+    }
 
     function calculatePrice(INFTMarket.MarketItem calldata item) override external view returns (uint256 sum) {
         PricingStrategies.PricingStrategy storage strategy = pricingStrategies.get(item.pricingStrategy);
